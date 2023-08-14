7e2b07a4d7b71cc4086236c6cce36da834e33d16
Priscilla <comptepro@mbpdepriscilla.home>
2023-08-14 14:04:35 +0200

    surcharge de fonction

--- Module2/main.js
+++ Module2/main.js
@@ -7,3 +7,16 @@ var adresse = {
     ville: "Mars",
     cp: 31000
 };
+var notes = [10, 16, 18];
+var prenoms = ["Mounya", "Joe", "Tya"];
+var presonnes = ["Mounya", 6];
+var perso = ["Mounya", 6, true];
+var tab = [
+    [10, 12],
+    [14, 46],
+    [18, 20]
+];
+var tab2 = [
+    { x: 1, y: 2 },
+    { x: 2, y: 3 }
+];
--- Module2/main.ts
+++ Module2/main.ts
@@ -8,3 +8,19 @@ const adresse  = {
     cp : 31000
 }
 
+const notes = [10,16,18];
+const prenoms  = ["Mounya", "Joe", "Tya"];
+const presonnes:(string|number)[] = ["Mounya",6];
+
+const perso:[string,number,boolean] = ["Mounya",6,true];
+
+const tab = [
+    [10,12],
+    [14,46],
+    [18,20]
+]
+
+const tab2 = [
+    {x:1,y:2},
+    {x:2,y:3}
+]
