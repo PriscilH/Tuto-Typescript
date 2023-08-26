c24af177805bc7813b6cfebb987b7aef4b59a9c4
Priscilla <comptepro@mbpdepriscilla.home>
2023-08-26 17:41:23 +0200

    intersection

--- Module2/main.js
+++ Module2/main.js
@@ -1,33 +1,10 @@
-var p1 = {
-    nom: "Mounya",
-    race: "Humain",
-    age: 6
+;
+var test = {
+    classe: "voleur"
 };
-var p2 = {
-    nom: "Albo",
-    race: "Monstre",
-    tribu: "Orc vert"
+var math = {
+    nom: "Math",
+    age: 31,
+    classe: "Guerrier",
+    sports: ["foot", "rugby"]
 };
-function afficherPersonnage(perso) {
-    console.log("Nom : " + perso.nom);
-    if (perso.race === "Humain") {
-        console.log("Age : " + perso.age);
-    }
-    if (perso.race === "Monstre") {
-        console.log("Tribu : " + perso.tribu);
-    }
-}
-afficherPersonnage(p1);
-afficherPersonnage(p2);
-function format(input) {
-    if (typeof input === "string") {
-        return "Text: ".concat(input);
-    }
-    else {
-        return "Number: ".concat(input.toString());
-    }
-}
-var formattedText = format("Hello");
-var formattedNumber = format(42);
-console.log(formattedText); // Text: Hello
-console.log(formattedNumber); // Number: 42
--- Module2/main.ts
+++ Module2/main.ts
@@ -1,58 +1,23 @@
-interface Personnage {
+interface Personnage  {
     nom : string;
-    race : string;
-}
-
-interface Humain extends Personnage  {
     age : number;
-    race : "Humain";
 }
 
-interface Monstre extends Personnage {
-    tribu : string;
-    race : "Monstre";
+interface Humain  {
+    classe : string;
 }
 
-type Perso= Humain | Monstre;
+interface Homme extends Personnage, Humain{
+    sports : string[],
+};
 
-const p1: Humain = {
-    nom : "Mounya",
-    race : "Humain",
-    age : 6
+const test : Humain = {
+    classe : "voleur"
 }
 
-const p2 : Monstre = {
-    nom : "Albo",
-    race : "Monstre",
-    tribu : "Orc vert"
+const math:Homme ={
+    nom : "Math",
+    age : 31,
+    classe : "Guerrier",
+    sports : ["foot", "rugby"]
 }
-
-function afficherPersonnage(perso : Perso){
-    console.log("Nom : " + perso.nom);
-    if(perso.race==="Humain"){
-      console.log("Age : " + perso.age);
-    }
-    if (perso.race==="Monstre"){
-        console.log("Tribu : " + perso.tribu);
-    }
-}
-
-afficherPersonnage(p1);
-afficherPersonnage(p2);
-
-// Exemple surcharge de fonction : 
-function format(input: string): string;
-function format(input: number): string;
-
-function format(input: string | number): string {
-  if (typeof input === "string") {
-    return `Text: ${input}`;
-  } else {
-    return `Number: ${input.toString()}`;
-  }
-}
-
-const formattedText = format("Hello");
-const formattedNumber = format(42);
-console.log(formattedText); // Text: Hello
-console.log(formattedNumber); // Number: 42
