41664eaa20ba7d3527744a24415300f0984e9d39
Priscilla <comptepro@mbpdepriscilla.home>
2024-06-19 00:16:49 +0200

    clean P1 folder

--- P1/dist/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const app = document.querySelector("#app");
-app.innerHTML = "<h1>CouCou</h1>";
-// tsc main.ts - génère un fichier js -> main.js
-//# sourceMappingURL=main.js.map
--- P1/src/main.ts
+++
@@ -1,4 +0,0 @@
-const app = document.querySelector("#app")!;
-app.innerHTML = "<h1>CouCou</h1>";
-
-// tsc main.ts - génère un fichier js -> main.js
