3a8c8e2ead44c0c84d785ffa67203d3815e7b808
Priscilla <comptepro@mbpdepriscilla.home>
2024-06-20 16:12:21 +0200

    add list options

--- P1/dist/main.js
+++ P1/dist/main.js
@@ -1,50 +1,39 @@
-type Devise = {
-    nom:string;
-    code:string;
-    symbole:string;
-    taux:number;
-}
-
-const dollar:Devise = {
+"use strict";
+const dollar = {
     nom: "Dollar",
     code: "Dol",
     symbole: "$",
     taux: 1
-}
-
-const euro:Devise = {
+};
+const euro = {
     nom: "Euro",
     code: "Eur",
     symbole: "€",
     taux: 0.93
-}
-
-const livre:Devise = {
+};
+const livre = {
     nom: "Livre",
     code: "Liv",
     symbole: "£",
     taux: 0.79
-}
-
-const yuan:Devise = {
+};
+const yuan = {
     nom: "Yuan",
     code: "Yua",
     symbole: "¥",
     taux: 7.27
-}
-
-const devises:Devise[] = [dollar,euro,livre,yuan];
+};
+const devises = [dollar, euro, livre, yuan];
 console.log(devises);
-
-const deviseSelect = document.querySelector("#devise-initiale")! as HTMLSelectElement;
+const deviseSelect = document.querySelector("#devise-initiale");
 deviseSelect.innerHTML = listOption(devises);
-
-const deviseFinSelect = document.querySelector("#devise-finale")! as HTMLSelectElement;
+const deviseFinSelect = document.querySelector("#devise-finale");
 deviseFinSelect.innerHTML = listOption(devises);
-function listOption(lesdevises:Devise[]) : string {
+function listOption(lesdevises) {
     let listDevisTxt = "";
-    for(let devise of lesdevises){
+    for (let devise of lesdevises) {
         listDevisTxt += `<option value="${devise.code}">${devise.nom} - (${devise.symbole})</option>`;
     }
     return listDevisTxt;
 }
+//# sourceMappingURL=main.js.map
