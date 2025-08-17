32515dc68afe6e586f3639c2dc7bd535607f352f
Priscilla <comptepro@mbpdepriscilla.home>
2025-08-17 13:10:39 +0200

    Héritage + abstract

--- Module6/main.ts
+++ Module6/main.ts
@@ -1,39 +1,14 @@
-class Toolbox{
-    public static afficherTab (tab : any[]){
-        for(let element of tab){
-            console.log(element);
-        }
-    }
-
-    public static calculMoyenne(...nombres:number[]){
-        let moyenne = 0;
-        for(let n of nombres){
-            moyenne += n;
-        }
-        return moyenne/nombres.length;
-    }
-}
-
-class ParcAuto {
-    public listeVoiture:Voiture[] = [];
-    // public listeBus:Bus[] = [];
-}
-
-class Voiture {
+abstract class Voiture {
     public static readonly TVA:number = 20;
     public static listeVoiture:Voiture[] = [];
    
     constructor(
-        private _marque : string,
-        private _modele : string,
-        private _couleur : string,
-        private _nbPortes : number,
-        public readonly annee : number
+        protected _marque : string,
+        protected _modele : string,
+        protected _couleur : string,
+        protected _nbPortes : number,
+        protected readonly _annee : number
         ){
-    }
-
-    public static getVoitures(){
-        //permet de récupérer les Voitures d'une base données.
     }
 
     public static ajouterVoitureListe(v: Voiture){
@@ -44,16 +19,9 @@ class Voiture {
         Voiture.listeVoiture.pop();
     }
 
-    public afficherVoiture(this:Voiture) {
-        console.log("Marque : " + this._marque);
-        console.log("Modèle : " + this._modele);
-        this.afficherCouleur();
-        console.log("Nombre de portes : " + this._nbPortes);
-        console.log("Annee de construction : " + this.annee);
-        console.log(Voiture.TVA)
-    }
+    abstract afficherVoiture(this:Voiture);
 
-    private afficherCouleur() {
+    protected afficherCouleur() {
         console.log("Couleur : " + this._couleur);
     }
 
@@ -67,91 +35,59 @@ class Voiture {
         console.log("Setter du modele")
         this._modele = newModele;
     }
-
-    /**
-     * GETTERS Standard
-     */
-    // public getMarque() : string{return this.marque;}
-    // public getModele() : string{return this.modele;}
-    // public getNbPortes() : number{return this.nbPortes};
-
-    /**
-     * SETTERS Standard
-     */
-    // public setMarque(marque:string) : Voiture {
-    //     if(marque === "Gepo" || marque === "Yotota"){
-    //         this.marque = marque;
-    //     }
-    //     return this;
-    // }
-    // public setModele(modele:string) : Voiture {
-    //     this.modele = modele;
-    //     return this;
-    // }
-    // public setNbPortes(nbPortes : number){
-    //     if(nbPortes === 5 || nbPortes === 3){
-    //         this.nbPortes = nbPortes;
-    //     }
-    // }
 }
 
-var v1 = new Voiture("Yotota","Ryias","rouge",5,2020);
-var v2 = new Voiture("Yotota","Risau","noire",3,2021);
+class VoitureCourse extends Voiture {
+
+    constructor(marque : string,
+        modele : string,
+        nbPortes : number,
+        annee : number,
+        private _vitesse : number){
+            super(marque,modele,"Bleue",nbPortes,annee);
+        }
+    
+    public afficherVitesse(){
+        console.log("La vitesse max est de : " + this._vitesse);
+    }
+
+    public afficherVoiture(this:VoitureCourse) {
+        console.log("Marque : " + this._marque);
+        console.log("Modèle : " + this._modele);
+        this.afficherCouleur();
+        console.log("Nombre de portes : " + this._nbPortes);
+        console.log("Annee de construction : " + this._annee);
+        console.log(Voiture.TVA);
+        this.afficherVitesse();
+    }
+}
+
+class VoitureAncienne extends Voiture{
+    constructor(marque : string,
+        modele : string,
+        nbPortes : number,
+        annee : number){
+            super(marque,modele,"Noire",nbPortes,annee);
+        }
+    
+    public afficherVoiture(this:VoitureAncienne) {
+        console.log("Marque : " + this._marque);
+        console.log("Modèle : " + this._modele);
+        console.log("Nombre de portes : " + this._nbPortes);
+        console.log("Annee de construction : " + this._annee);
+    }
+}
+
+var v1 = new VoitureCourse("Yotota","Ryias",5,2020,300);
+var v2 = new VoitureCourse("Yotota","Risau",3,2021,200);
+var v3 = new VoitureAncienne("Yotota","Ryias",5,1999)
+v1.afficherVoiture();
+console.log("-------------------")
+v2.afficherVoiture();
+console.log("-------------------")
+v3.afficherVoiture();
 
 Voiture.ajouterVoitureListe(v1);
 Voiture.ajouterVoitureListe(v2);
 
-Toolbox.afficherTab(Voiture.listeVoiture);
 
-let notes = [ 10 , 15, 20];
-
-Toolbox.afficherTab(notes);
-
-console.log(Toolbox.calculMoyenne(10,15,20,25,100,200));
-
-// v1.afficherVoiture();
-// v2.afficherVoiture();
-
-// var parcMGA = new ParcAuto();
-// parcMGA.listeVoiture.push(v1,v2);
-// console.log(parcMGA.listeVoiture);
-
-
-
-// ... Utilisation de This ... //
-
-// class Voiture {
-//     marque;
-//     modele;
-//     couleur;
-//     nbPortes;
-
-//     constructor(marque,modele,couleur,nbPortes){
-//         this.marque = marque;
-//         this.modele = modele;
-//         this.couleur = couleur;
-//         this.nbPortes = nbPortes;
-//     }
-
-//     afficherVoiture(this:Voiture) {
-//         console.log("Marque : " + this.marque);
-//         console.log("Modèle : " + this.modele);
-//         this.afficherCouleur();
-//         console.log("Nombre de portes : " + this.nbPortes);
-//     }
-
-//     afficherCouleur() {
-//         console.log("Couleur : " + this.couleur);
-//     }
-// }
-
-// var v1 = new Voiture("Yotota","Ryias","rouge",5);
-// var v2 = new Voiture("Yotota","Risau","noire",3);
-// v1.afficherVoiture();
-// console.log("-----------------------");
-// v2.afficherVoiture();
-
-// let affichages = {
-//     afficherVoiture : v1.afficherVoiture.bind(v1)
-// }
-// affichages.afficherVoiture();
