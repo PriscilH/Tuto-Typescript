2628367a695494c37b6283ab10df767008a6cc39
Priscilla <comptepro@mbpdepriscilla.home>
2025-08-17 12:34:14 +0200

    Utilisation de static

--- Module6/main.ts
+++ Module6/main.ts
@@ -1,4 +1,27 @@
+class Toolbox{
+    public static afficherTab (tab : any[]){
+        for(let element of tab){
+            console.log(element);
+        }
+    }
+
+    public static calculMoyenne(...nombres:number[]){
+        let moyenne = 0;
+        for(let n of nombres){
+            moyenne += n;
+        }
+        return moyenne/nombres.length;
+    }
+}
+
+class ParcAuto {
+    public listeVoiture:Voiture[] = [];
+    // public listeBus:Bus[] = [];
+}
+
 class Voiture {
+    public static readonly TVA:number = 20;
+    public static listeVoiture:Voiture[] = [];
    
     constructor(
         private _marque : string,
@@ -9,12 +32,25 @@ class Voiture {
         ){
     }
 
+    public static getVoitures(){
+        //permet de récupérer les Voitures d'une base données.
+    }
+
+    public static ajouterVoitureListe(v: Voiture){
+        Voiture.listeVoiture.push(v);
+    }
+
+    public static retirerVoitureListe(){
+        Voiture.listeVoiture.pop();
+    }
+
     public afficherVoiture(this:Voiture) {
         console.log("Marque : " + this._marque);
         console.log("Modèle : " + this._modele);
         this.afficherCouleur();
         console.log("Nombre de portes : " + this._nbPortes);
         console.log("Annee de construction : " + this.annee);
+        console.log(Voiture.TVA)
     }
 
     private afficherCouleur() {
@@ -62,8 +98,23 @@ class Voiture {
 var v1 = new Voiture("Yotota","Ryias","rouge",5,2020);
 var v2 = new Voiture("Yotota","Risau","noire",3,2021);
 
-v1.afficherVoiture();
-v2.afficherVoiture();
+Voiture.ajouterVoitureListe(v1);
+Voiture.ajouterVoitureListe(v2);
+
+Toolbox.afficherTab(Voiture.listeVoiture);
+
+let notes = [ 10 , 15, 20];
+
+Toolbox.afficherTab(notes);
+
+console.log(Toolbox.calculMoyenne(10,15,20,25,100,200));
+
+// v1.afficherVoiture();
+// v2.afficherVoiture();
+
+// var parcMGA = new ParcAuto();
+// parcMGA.listeVoiture.push(v1,v2);
+// console.log(parcMGA.listeVoiture);
 
 
 
