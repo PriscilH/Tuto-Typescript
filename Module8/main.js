b6fbf4baf0f71b298d0667d4b3441b842c286e5a
Priscilla <comptepro@mbpdepriscilla.home>
2025-08-19 16:59:33 +0200

    type partial and readonly

--- Module8/main.js
+++ Module8/main.js
@@ -1,11 +1,37 @@
-function creerPerso(obj1, obj2) {
-    return Object.assign(obj1, obj2);
-}
-const perso = { nom: "matthieu", age: 31 };
-const guerrier = { classe: "guerrier", tribu: "Orc vert" };
-const perso2 = { nom: "Tya", age: 18 };
-const archer = { classe: "archer", nbFleches: 50 };
-const g1 = creerPerso(perso, guerrier);
-const g2 = creerPerso(perso2, archer);
-const a = creerPerso({ nom: "Tya", age: 31 }, { classe: "fgdsgf" });
-console.log(a);
+var noms = ["tya", "Toto"];
+var p1 = {
+    nom: "Tya",
+    age: 15
+};
+// interface Personnage {nom : string;age : number;}
+// interface Guerrier extends Personnage {classe : "guerrier";tribu : string;}
+// interface Archer extends Personnage  {classe : "archer";nbFleches : number;}
+// const g1:Guerrier = {nom : "Milo",age:18,classe:"guerrier",tribu:"Orc vert"};
+// const a1:Archer = {nom:"Tya",age:18,classe:"archer",nbFleches:50};
+// class Personnages<T extends Personnage>{
+//     private liste:Array<T> = [];
+//     public ajouterPersonnage(perso:T){
+//         this.liste.push(perso);
+//     }
+//     public afficherPersonnages(){
+//         for(let element of this.liste){
+//             console.log(element);
+//         }
+//     }
+// }
+// const listeGuerrier = new Personnages<Guerrier>();
+// listeGuerrier.ajouterPersonnage(g1);
+// console.log("Liste de guerriers :");
+// listeGuerrier.afficherPersonnages();
+// const listeArcher = new Personnages<Archer>();
+// listeArcher.ajouterPersonnage(a1);
+// console.log("Liste d'archers :");
+// listeArcher.afficherPersonnages();
+// function creerPerso() : Personnage {
+//     let p1:Partial<Personnage> = {
+//         nom : "Milo",
+//     }
+//     p1.age = 15;
+//     return p1 as Personnage;
+// }
+// console.log(p1);
--- Module8/main.ts
+++ Module8/main.ts
@@ -1,28 +1,53 @@
-function creerPerso<T extends Personnage,U extends {classe:string}>(obj1:T,obj2:U){
-    return Object.assign(obj1,obj2);
-}
+const noms : Readonly<string[]> = ["tya" , "Toto"];
 
-interface Personnage {
+type Personnage = {
     nom : string;
     age : number;
 }
-interface Guerrier {
-    classe : "guerrier";
-    tribu : string;
-}
-interface Archer {
-    classe : "archer";
-    nbFleches : number;
+
+const p1:Readonly<Personnage> = {
+    nom :"Tya",
+    age : 15
 }
 
-const perso:Personnage = {nom : "matthieu",age : 31}
-const guerrier:Guerrier = {classe : "guerrier",tribu : "Orc vert"}
 
-const perso2 : Personnage = {nom : "Tya",age:18};
-const archer : Archer = {classe:"archer",nbFleches:50}
+// interface Personnage {nom : string;age : number;}
+// interface Guerrier extends Personnage {classe : "guerrier";tribu : string;}
+// interface Archer extends Personnage  {classe : "archer";nbFleches : number;}
 
-const g1 = creerPerso(perso,guerrier);
-const g2 = creerPerso(perso2,archer);
+// const g1:Guerrier = {nom : "Milo",age:18,classe:"guerrier",tribu:"Orc vert"};
+// const a1:Archer = {nom:"Tya",age:18,classe:"archer",nbFleches:50};
 
-const a = creerPerso({nom:"Tya",age:31},{classe:"fgdsgf"});
-console.log(a);
+// class Personnages<T extends Personnage>{
+//     private liste:Array<T> = [];
+
+//     public ajouterPersonnage(perso:T){
+//         this.liste.push(perso);
+//     }
+
+//     public afficherPersonnages(){
+//         for(let element of this.liste){
+//             console.log(element);
+//         }
+//     }
+// }
+
+// const listeGuerrier = new Personnages<Guerrier>();
+// listeGuerrier.ajouterPersonnage(g1);
+// console.log("Liste de guerriers :");
+// listeGuerrier.afficherPersonnages();
+// const listeArcher = new Personnages<Archer>();
+// listeArcher.ajouterPersonnage(a1);
+// console.log("Liste d'archers :");
+// listeArcher.afficherPersonnages();
+
+
+// function creerPerso() : Personnage {
+//     let p1:Partial<Personnage> = {
+//         nom : "Milo",
+//     }
+//     p1.age = 15;
+//     return p1 as Personnage;
+// }
+
+// console.log(p1);
