6a6da4e987c628684a583e88671ace4c4aab30a7
Priscilla <comptepro@mbpdepriscilla.home>
2025-08-19 16:48:52 +0200

    paramètres de fonctions

--- Module8/main.js
+++ Module8/main.js
@@ -1,19 +1,11 @@
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
-var h1 = { nom: "Tya", age: 18 };
-var o1 = { nom: "Gael", tribu: "Orc vert" };
-var g1 = creerGuerrier(h1);
-var g2 = creerGuerrier(o1);
-var g3 = creerGuerrier({ nom: "toto", sexe: true });
-function creerGuerrier(j) {
-    return __assign(__assign({}, j), { classe: "Guerrier" });
+function creerPerso(obj1, obj2) {
+    return Object.assign(obj1, obj2);
 }
+const perso = { nom: "matthieu", age: 31 };
+const guerrier = { classe: "guerrier", tribu: "Orc vert" };
+const perso2 = { nom: "Tya", age: 18 };
+const archer = { classe: "archer", nbFleches: 50 };
+const g1 = creerPerso(perso, guerrier);
+const g2 = creerPerso(perso2, archer);
+const a = creerPerso({ nom: "Tya", age: 31 }, { classe: "fgdsgf" });
+console.log(a);
--- Module8/main.ts
+++ Module8/main.ts
@@ -1,17 +1,28 @@
-type Humain = {nom:string,age:number};
-type Orc = {nom : string, tribu:string};
-
-let h1:Humain = {nom:"Tya",age:18};
-let o1:Orc = {nom:"Gael",tribu:"Orc vert"};
-
-let g1 = creerGuerrier(h1);
-let g2 = creerGuerrier(o1);
-
-let g3 = creerGuerrier({nom:"toto",sexe:true});
-
-function creerGuerrier<Type extends{nom:string}>(j:Type){
-    return {
-        ...j,
-        classe:"Guerrier"
-    }
+function creerPerso<T extends Personnage,U extends {classe:string}>(obj1:T,obj2:U){
+    return Object.assign(obj1,obj2);
 }
+
+interface Personnage {
+    nom : string;
+    age : number;
+}
+interface Guerrier {
+    classe : "guerrier";
+    tribu : string;
+}
+interface Archer {
+    classe : "archer";
+    nbFleches : number;
+}
+
+const perso:Personnage = {nom : "matthieu",age : 31}
+const guerrier:Guerrier = {classe : "guerrier",tribu : "Orc vert"}
+
+const perso2 : Personnage = {nom : "Tya",age:18};
+const archer : Archer = {classe:"archer",nbFleches:50}
+
+const g1 = creerPerso(perso,guerrier);
+const g2 = creerPerso(perso2,archer);
+
+const a = creerPerso({nom:"Tya",age:31},{classe:"fgdsgf"});
+console.log(a);
